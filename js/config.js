72acac875a16498a69533e6f084b0e35a381d086
satoshitakuma <134661372+satoshitakuma@users.noreply.github.com>
2023-05-29 10:50:45 +0900

    new_config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_N97669aZs', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '55n28smudfh7jg3jv2lp9672qq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
